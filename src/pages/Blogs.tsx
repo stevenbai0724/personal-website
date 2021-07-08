0761eeef40c7c70923c08deb312e7b3cb0faecdf
stevenbai0724 <stevenbai0724@gmail.com>
2021-07-08 11:46:55 -0400

    July 8

--- src/pages/Blogs.tsx
+++ src/pages/Blogs.tsx
@@ -3,7 +3,8 @@ import React from 'react';
 const Blogs = () =>{ 
     return(
         <div>
-            <h1>Blogs coming soon! </h1>
+            <h1 style = {h1Style}>Blogs coming soon! </h1>
+            
             <br></br>
             <br></br>
             <br></br>
@@ -23,6 +24,28 @@ const Blogs = () =>{
         </div>
     )
 
+}
+const h1Style: React.CSSProperties = {
+    textAlign: 'center',
+}
+const h2Style: React.CSSProperties = {
+    textAlign: 'center',
+    fontWeight: 'normal',
+    fontSize: '25px',
+}
+const h3Style: React.CSSProperties = {
+    textAlign: 'left',
+    fontWeight: 'normal',
+    fontSize: '20px',
+    marginLeft: '28%',
+    marginRight: '27%',
+}
+const imgStyle: React.CSSProperties = {
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: '50%',
+   
 }
 
 export default Blogs;
--- src/pages/Projects.tsx
+++ src/pages/Projects.tsx
@@ -3,7 +3,9 @@ import React from 'react';
 const Projects = () =>{ 
     return(
         <div>
-            <h1>Projects coming soon! </h1>
+            <h1 style = {h1Style}>More projects coming soon! </h1>
+            <a href = "https://devpost.com/stevenbai0724"><h2 style = {h2Style}>Devpost</h2></a>
+            <a href = "https://dmoj.ca/user/stevenbai0724"><h2 style = {h2Style}>DMOJ</h2></a>
             <br></br>
             <br></br>
             <br></br>
@@ -15,7 +17,7 @@ const Projects = () =>{
             <br></br>
             <br></br>
             <br></br>
-            <br></br><br></br>
+            <br></br>
             <br></br>
             
 
@@ -23,6 +25,29 @@ const Projects = () =>{
         </div>
     )
 
+}
+const h1Style: React.CSSProperties = {
+    textAlign: 'center',
+}
+const h2Style: React.CSSProperties = {
+    textAlign: 'center',
+    fontWeight: 'normal',
+    fontSize: '25px',
+    color: 'white',
+}
+const h3Style: React.CSSProperties = {
+    textAlign: 'left',
+    fontWeight: 'normal',
+    fontSize: '20px',
+    marginLeft: '28%',
+    marginRight: '27%',
+}
+const imgStyle: React.CSSProperties = {
+    display: 'block',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    width: '50%',
+   
 }
 
 export default Projects;
