7a0803840ee85db9f8d94119753b9ece5ff70348
Steven <stevenbai0724@gmail.com>
2022-04-24 20:07:43 -0400

    project modal on click

--- src/components/ProjectCard.tsx
+++ src/components/ProjectCard.tsx
@@ -99,7 +99,7 @@ const useStyles = makeStyles((theme) =>  ({
         maxWidth: "80vw",
         textAlign: "center",
         overflowY: "auto",
-        maxHeight: "calc(100vh - 100px)",
+        maxHeight: "calc(100vh - 150px)",
         '&::-webkit-scrollbar' :{
             width: '4px',
 
